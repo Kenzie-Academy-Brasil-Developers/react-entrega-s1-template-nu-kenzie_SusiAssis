b7acc4f522a221ccceb76a621855846d5a952100
Susilane <susi.planos@gmail.com>
2023-03-24 23:29:24 -0300

    EntregaNuKenzie

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -4,7 +4,9 @@ import logo from "../../assets/NuKenzie.png"
 const Header = ()=>{
     return(
         <header className={styles.header}>
+            <div className={styles.img}>
            <img src={logo} alt="logo NuKenzie" />
+            </div>
         </header>
     )
 }
