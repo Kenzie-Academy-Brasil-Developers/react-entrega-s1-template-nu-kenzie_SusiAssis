2de8f118b75c0f4a30a9d574b80343bd206485dc
Susilane <susi.planos@gmail.com>
2023-03-27 08:32:14 -0300

    EntregaNuKenzie

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -24,7 +24,7 @@ const Form = ({listTransactions, setListTransactions})=>{
 
         transactions.description = ""
         transactions.declaredValue = ""
-        transactions.valueType = ""
+        
      }
     
     
